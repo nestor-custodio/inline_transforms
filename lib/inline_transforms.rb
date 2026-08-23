1ea46b927648906bb100d17d6ca774bcc4ee2e4f
Nestor Custodio <nestor@custodio.org>
2026-08-22 20:17:31 -0500

    style: initial `rubocop` pass

--- lib/inline_transforms.rb
+++ lib/inline_transforms.rb
@@ -1,6 +1,4 @@
-# frozen_string_literal: true
-
-require_relative "inline_transforms/version"
+require_relative 'inline_transforms/version'
 
 module InlineTransforms
   class Error < StandardError; end
--- spec/inline_transforms_spec.rb
+++ spec/inline_transforms_spec.rb
@@ -1,11 +1,9 @@
-# frozen_string_literal: true
-
 RSpec.describe InlineTransforms do
-  it "has a version number" do
+  it 'has a version number' do
     expect(InlineTransforms::VERSION).not_to be nil
   end
 
-  it "does something useful" do
+  it 'does something useful' do
     expect(false).to eq(true)
   end
 end
