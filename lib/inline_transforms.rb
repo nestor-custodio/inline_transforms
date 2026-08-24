632a8d62bc67b4667f1d2c2e044b6ebbc790330b
Nestor Custodio <nestor@custodio.org>
2026-08-24 12:22:30 -0500

    fix: `Proc#===` doesn't do what you expect  (╯°□°）╯ A ┻━┻

--- lib/inline_transforms.rb
+++ lib/inline_transforms.rb
@@ -11,7 +11,7 @@ module InlineTransforms
   # This takes an option `then: nil`, but is defined with `**options` because `then` is reserved.
   #
   def unless(bad_value, **options)
-    return self unless bad_value === self
+    return self unless inline_transforms_equality? bad_value
 
     alt_value = options[:then]
     return alt_value.call if alt_value.is_a? Proc
@@ -24,8 +24,19 @@ module InlineTransforms
   def transform(**options)
     default = options.delete :else
 
-    options.each { |key, value| return value.unless Proc, then: -> { value.call } if key === self }
+    options.each { |key, value| return value.unless Proc, then: -> { value.call } if inline_transforms_equality? key }
     default
+  end
+
+  private
+
+  # You can't case-compare *against* a `Proc` because `Proc#===` is an alias for `Proc#call`. (?!)
+  # So we have to be careful when testing for this.
+  #
+  def inline_transforms_equality?(value)
+    return value == self if value.is_a? Proc
+
+    value === self
   end
 
   # rubocop:enable Style/CaseEquality
