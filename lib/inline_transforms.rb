c2582413c6eef6d27e31cb41b88c1d55e5b00937
Nestor Custodio <nestor@custodio.org>
2026-08-25 03:21:00 -0500

    docs: flesh out the `update`/`transform` descriptions

--- lib/inline_transforms.rb
+++ lib/inline_transforms.rb
@@ -8,7 +8,20 @@ module InlineTransforms
   #
   # rubocop:disable Style/CaseEquality
 
-  # This takes an option `then: nil`, but is defined with `**options` because `then` is reserved.
+  # Returns `self` ... unless it matches a "bad" value, in which case it returns an alternate (`then:`) value.
+  #
+  # (NOTE: this takes a `then: nil` named param, but is defined as taking `(**options)` because `then` is reserved.)
+  #
+  #
+  # @param bad_value [Object]
+  #   The one value we consider "bad", and for which we'd like the `then:` option back instead.
+  #
+  # @option then: [Object]
+  #   The value we'd like back if `self` matches the `bad_value` **using case comparison**.
+  #   Defaults to `nil`.
+  #
+  # @return
+  #   Returns either `self` or the value given as the `then:` option.
   #
   def unless(bad_value, **options)
     return self unless inline_transforms_equality? bad_value
@@ -19,7 +32,20 @@ module InlineTransforms
     alt_value
   end
 
-  # This takes an option `else: nil`, but is defined with `**options` because `else` is reserved.
+  # Returns `self` ... unless it matches one of the keys in the given "transformation hash", in which case the
+  # corresponding hash value is returned. If no matching hash key is found, this returns a fallback (`else:`) value.
+  #
+  # (NOTE: this takes an `else: nil` named param, but is defined as taking `(**options)` because `else` is reserved.)
+  #
+  #
+  # @option **
+  #   A hash of keys to case-compare against `self` so we can return the corresponding value.
+  #
+  # @option else: [Object]
+  #   The fallback value to return if no matching key is found.
+  #
+  # @return
+  #   Returns either `self`, one of the transformation hash values, or the `else:` option.
   #
   def transform(**options)
     default = options.delete :else
@@ -33,6 +59,11 @@ module InlineTransforms
   # You can't case-compare *against* a `Proc` because `Proc#===` is an alias for `Proc#call`. (?!)
   # So we have to be careful when testing for this.
   #
+  # @param value [Object]
+  #   The value against which to case-compare `self`.
+  #
+  # @return [true, false]
+  #
   def inline_transforms_equality?(value)
     return value == self if value.is_a? Proc
 
@@ -42,6 +73,6 @@ module InlineTransforms
   # rubocop:enable Style/CaseEquality
 end
 
-# Add these methods to all `Object`s.
+# Gets the `InlineTransforms` methods.
 #
 class Object; include InlineTransforms; end # rubocop:disable Style/OneClassPerFile
